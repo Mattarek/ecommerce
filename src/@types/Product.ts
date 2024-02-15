b150b6bd8e2439433aeb6ed2a71fa8096a01f315
Marcin Barc <marb637@gmail.com>
2024-02-15 17:43:28 +0100

    Init files from vite added

---
+++ src/@types/Product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    name: string;
+    slug: string;
+    image: string;
+    category: string;
+    brand: string;
+    price: number;
+    countInStock: number;
+    descripion: string;
+    rating: number;
+    numReviews: number;
+}
